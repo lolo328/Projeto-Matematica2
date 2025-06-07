1c8c3555492b7e13450043f13700bec85659f796
lolo328 <o.lorhayne@escola.pr.gov.br>
2025-06-07 01:15:09 +0000

    Trabalho Pronto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital da Argélia?',
+    'A capital é Luanda'
+)
+
+criaCartao(
+    'História',
+    'Qual a data que começou a segunda Guerra Mundial?',
+    '1 de setembro de 1939'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual o maior time do Mundo?',
+    'Real Madrid'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz viajar em Inglês?',
+    'Viajar em ingles é Travel'
+)
